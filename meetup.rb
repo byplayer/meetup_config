18ba8187f3a2b69694cfba506aa3c6bb3b5c34bf
byplayer <byplayer100@gmail.com>
2014-04-20 23:52:59 +0800

    implement conf

--- meetup.rb
+++ meetup.rb
@@ -1,12 +1,19 @@
+# Sample class to load configuration
 class Meetup
+  attr_accessor :group, :organizer, :sponsors
+
   def self.load(path)
-    
-  end
+    fail "config file not found: #{path}" unless File.exist?(path)
 
-  class MeetupDsl
-    def initialize(config)
-      
+    mt = Meetup.new
+    File.open(path, 'r') do |file|
+      mt.instance_eval(File.read(path), path)
     end
+
+    mt
   end
 
+  def meetup
+    yield self
+  end
 end
